24a68620fa1755d0d944484b5e482638fee3ada3
Al Murray <2515630+rogueyoshi@users.noreply.github.com>
2022-03-06 14:34:32 -0500

    Update TournamentSet.svelte

--- src/lib/TournamentSet.svelte
+++ src/lib/TournamentSet.svelte
@@ -22,7 +22,7 @@
 {#if set}
 	<div class="fixed left-0 right-0 grid justify-items-center text-sx text-center">
 		<div>
-			<div class="bg-white text-4xl outline-dashed outline-black">
+			<div class="bg-white text-4xl  border-2 border-black border-dashed border-b-0">
 				<span>{set.event.tournament.name}</span>
 				-
 				<span>{set.event.name}</span>
@@ -32,39 +32,39 @@
 
 			<div class="h-16 flex justify-center">
 				<img
-					class="[height:125%] aspect-square object-cover drop-shadow-2xl rounded-b-xl bg-black outline-dashed outline-black"
-					src={set.slots[0].entrant.participants[0].user.images?.[0]?.url ?? '/SX Icon White.webp'}
+					class="[height:125%] aspect-square object-cover drop-shadow-2xl rounded-b-xl bg-white border-2 border-black border-dashed"
+					src={set.slots[0].entrant.participants[0].user.images?.[0]?.url ?? '/SX Icon Black.webp'}
 					alt={set.slots[0].entrant.name}
 				/>
 				<span
-					class="bg-red-700 w-80 p-4 grid justify-center content-center text-white text-4xl font-bold outline-dashed outline-black"
+					class="bg-red-700 w-80 p-4 grid justify-center content-center text-white text-4xl font-bold border-2 border-black border-dashed"
 				>
 					{set.slots[0].entrant.name}</span
 				>
 				<span
-					class="bg-white grid justify-center content-center aspect-square font-bold text-6xl outline-dashed outline-black"
+					class="bg-white grid justify-center content-center aspect-square font-bold text-6xl border-2 border-x-0 border-black border-dashed"
 				>
 					{set.slots[0].standing.stats.score.value ?? 0}
 				</span>
 				<img
-					class="bg-white [height:150%] object-scale-down drop-shadow-2xl rounded-b-2xl outline-dashed outline-black"
+					class="bg-white [height:150%] object-scale-down drop-shadow-2xl rounded-b-2xl border-2 border-black border-dashed"
 					src="/SX Icon Black.webp"
 					alt="SX Logo"
 				/>
 				<!-- <span class="bg-white  grid justify-center items-center font-thin text-4xl"> | </span> -->
 				<span
-					class="bg-white grid justify-center content-center aspect-square font-black text-6xl outline-dashed outline-black"
+					class="bg-white grid justify-center content-center aspect-square font-black text-6xl border-2 border-x-0 border-black border-dashed"
 				>
 					{set.slots[1].standing.stats.score.value ?? 0}
 				</span>
 				<span
-					class="bg-red-700 w-80 p-4 grid justify-center content-center text-white text-4xl font-bold outline-dashed outline-black"
+					class="bg-red-700 w-80 p-4 grid justify-center content-center text-white text-4xl font-bold border-2 border-black border-dashed"
 				>
 					{set.slots[1].entrant.name}
 				</span>
 				<img
-					class="[height:125%] aspect-square object-cover drop-shadow-2xl rounded-b-xl bg-black outline-dashed outline-black"
-					src={set.slots[1].entrant.participants[0].user.images?.[0]?.url ?? '/SX Icon White.webp'}
+					class="[height:125%] aspect-square object-cover drop-shadow-2xl rounded-b-xl bg-white border-2 border-black border-dashed"
+					src={set.slots[1].entrant.participants[0].user.images?.[0]?.url ?? '/SX Icon Black.webp'}
 					alt={set.slots[1].entrant.name}
 				/>
 			</div>
