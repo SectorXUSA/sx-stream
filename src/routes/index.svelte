fdfd7eaf6df14c4b5a80982a6d48bb143eb1c139
Al Murray <2515630+rogueyoshi@users.noreply.github.com>
2022-03-10 20:05:03 -0500

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,4 @@
-<a href="/smashgg/sx-stream-test/dashboard"><div>Dashboard (Configs and Overrides)</div></a>
 <a href="/smashgg/sx-stream-test/overlay"><div>Overlay (Use in OBS Browser Source, etc.)</div></a>
+<a href="/smashgg/sx-stream-test/string.txt"><div>VOD string (For use in YouTube, etc.)</div></a>
+<a href="/smashgg/sx-stream-test/dashboard"><div>Dashboard (Configs and Overrides)</div></a>
 <a href="https://github.com/SectorXUSA/sx-stream"><div>Source Code</div></a>
