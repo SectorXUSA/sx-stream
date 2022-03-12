dad005d21a1c2cc37580dd74efacecb2c9822577
Al Murray <2515630+rogueyoshi@users.noreply.github.com>
2022-03-12 17:40:08 -0500

    Start supabase implementation

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,18 @@
 <script>
+	import { createClient } from '@supabase/supabase-js';
 	import { initClient } from '@urql/svelte';
 
+	// Supabase
+	const supabaseUrl = import.meta.env.VITE_SUPABASE_URL?.toString();
+	const supabaseKey = import.meta.env.VITE_SUPABASE_KEY?.toString();
+	const supabase = createClient(supabaseUrl, supabaseKey);
+
+	// Urql
 	initClient({
 		url: import.meta.env.VITE_SMASHGG_URL?.toString(),
 		fetchOptions: () => {
 			const token = import.meta.env.VITE_SMASHGG_TOKEN?.toString();
+
 			return {
 				headers: { authorization: token ? `Bearer ${token}` : '' }
 			};
---
+++ src/routes/smashgg/[slug]/dashboard.svelte
@@ -0,0 +1,55 @@
+<form class="prose antialiased min-w-full">
+	<fieldset>
+		<legend>Set</legend>
+		<label class="block">
+			<span>fullRoundText</span>
+			<input type="text" />
+		</label>
+		<label class="block">
+			<span>eventName</span>
+			<input type="text" />
+		</label>
+		<label class="block">
+			<span>tournamentName</span>
+			<input type="text" />
+		</label>
+		<label class="block">
+			<span>videogameName</span>
+			<input type="text" />
+		</label>
+	</fieldset>
+	<fieldset>
+		<legend>Player 1</legend>
+		<label class="inline">
+			<span>prefix</span>
+			<input type="text" />
+		</label>
+		<label class="block">
+			<span>gamerTag</span>
+			<input type="text" />
+		</label>
+		<label class="block">
+			<span>score</span>
+			<input type="number" />
+			<button>-</button>
+			<button>+</button>
+		</label>
+	</fieldset>
+	<fieldset>
+		<legend>Player 2</legend>
+		<label class="block">
+			<span>prefix</span>
+			<input type="text" />
+		</label>
+		<label class="block">
+			<span>gamerTag</span>
+			<input type="text" />
+		</label>
+		<label class="block">
+			<span>score</span>
+			<input type="number" />
+			<button>-</button>
+			<button>+</button>
+		</label>
+	</fieldset>
+</form>
