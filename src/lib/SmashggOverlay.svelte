7d80b6b5aacd681bfedae277e2c0fedd33c7811c
Al Murray <2515630+rogueyoshi@users.noreply.github.com>
2022-03-12 00:47:37 -0500

    Update SmashggOverlay.svelte

--- src/lib/SmashggOverlay.svelte
+++ src/lib/SmashggOverlay.svelte
@@ -83,7 +83,7 @@
 					{#if set.slots[1].entrant.participants[0].prefix}
 						<img
 							class="drop-shadow-2xl object-cover w-full h-full"
-							src="/logos/${set.slots[1].entrant.participants[0].prefix}.png"
+							src="/logos/{set.slots[1].entrant.participants[0].prefix}.png"
 							alt={set.slots[1].entrant.participants[0].prefix}
 							on:error={(e) => {
 								e.target.src = '/logos/_fallback.png';
